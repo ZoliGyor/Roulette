9a496034161f5370dea7cafcea056dd95b3f7ab5
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-20 18:34:01 +0100

    Created the constructor for the Wheel and printed out the numbers

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Wheel roulette = new Wheel();
+        roulette.printOutNumbers();
+    }
 }
--- Wheel.java
+++ Wheel.java
@@ -1,5 +1,24 @@
 public class Wheel {
-    private int[] numbers;
+    private String[] numbers = new String[38];
 
-    
+    public Wheel()
+    {
+        numbers[0] = "0";
+        numbers[1] = "00";
+        for(int i = 2; i < numbers.length; i++){
+            numbers[i] = String.valueOf(i-1);
+        }
+    }
+
+    public void printOutNumbers()
+    {
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+    }
+
+    public String[] getNumbers()
+    {
+        return numbers;
+    }
 }
