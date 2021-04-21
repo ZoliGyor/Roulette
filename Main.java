fd9167229978b15ab8ef030747da5d8c0f1e385c
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-21 13:45:51 +0100

    Created the high numbers option, an ending where you run out of money, and if the player wants to bet a bigger than his current money

--- Main.java
+++ Main.java
@@ -20,27 +20,43 @@ public class Main {
             }else if(input.equals("bet")){
                 System.out.println("How much money would you like to bet?");
                 currentBet = numberScanner.nextInt();
-                player.setCash(player.getCash() - currentBet);
-                System.out.println("What would you like to bet on? \nTo bet on a single number write: single\nTo bet on low numbers (1-18) write: low");
-                System.out.println("To bet on high numbers (19-38) write: high\nTo bet on Red numbers write: red\nTo bet on black numbers write: balck");
-                System.out.println("To bet on even numbers write: even\nTo bet on odd numbers write odd");
-                input = scanner.nextLine();
-                if(input.equals("single")){
-                    System.out.println("What number would you like to choose?");
+                if(currentBet > player.getCash()){
+                    System.out.println("I am sorry but you do not have that much money");
+                }else{
+                    player.setCash(player.getCash() - currentBet);
+                    System.out.println("What would you like to bet on? \nTo bet on a single number write: single\nTo bet on low numbers (1-18) write: low");
+                    System.out.println("To bet on high numbers (19-38) write: high\nTo bet on Red numbers write: red\nTo bet on black numbers write: balck");
+                    System.out.println("To bet on even numbers write: even\nTo bet on odd numbers write odd");
                     input = scanner.nextLine();
-                    if(input.equals(roulette.rollTheWheelForNumbers())){
-                        System.out.println("Congratulations! You won 35 times the amount of your bet! This means: " + (currentBet*35) + " pounds");
-                        player.setCash(player.getCash() + (currentBet*35));
-                    }else{
-                        System.out.println("Awwww, you lost this round");
+                    if(input.equals("single")){
+                        System.out.println("What number would you like to choose?");
+                        input = scanner.nextLine();
+                        if(input.equals(roulette.rollTheWheelForNumbers())){
+                            System.out.println("Congratulations! You won 35 times the amount of your bet! This means: " + (currentBet*35) + " pounds");
+                            player.setCash(player.getCash() + (currentBet*35));
+                        }else{
+                            System.out.println("Awwww, you lost this round");
+                        }
+                    }else if(input.equals("low")){
+                        Integer numberAfterTheRoll = Integer.valueOf(roulette.rollTheWheelForNumbers());
+                        if(numberAfterTheRoll < 18 && numberAfterTheRoll > 0){
+                            System.out.println("Congratulations! You won 2 times the amount of your bet! This means: " + (currentBet*2) + " pounds");
+                            player.setCash(player.getCash() + (currentBet*2));
+                        }else{
+                            System.out.println("Awwww, you lost this round");
+                        }
+                    }else if(input.equals("high")){
+                        Integer numberAfterTheRoll = Integer.valueOf(roulette.rollTheWheelForNumbers());
+                        if(numberAfterTheRoll > 18){
+                            System.out.println("Congratulations! You won 2 times the amount of your bet! This means: " + (currentBet*2) + " pounds");
+                            player.setCash(player.getCash() + (currentBet*2));
+                        }else{
+                            System.out.println("Awwww, you lost this round");
+                        }
                     }
-                }else if(input.equals("low")){
-                    Integer numberAfterTheRoll = Integer.valueOf(roulette.rollTheWheelForNumbers());
-                    if(numberAfterTheRoll < 18 && numberAfterTheRoll > 0){
-                        System.out.println("Congratulations! You won 2 times the amount of your bet! This means: " + (currentBet*2) + " pounds");
-                        player.setCash(player.getCash() + (currentBet*2));
-                    }else{
-                        System.out.println("Awwww, you lost this round");
+                    if(player.getCash() <= 0){
+                        System.out.println("Awwww, it looks like you don't have any money left...Well do make sure to come back next time!");
+                        break;
                     }
                 }
             }
